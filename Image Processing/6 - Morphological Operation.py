f29d067d472187ce628a915ebfa7f085f7617432
KeerthiVasan.ai <keerthi77459@gmail.com>
2023-08-03 13:03:23 +0530

    Exercise 5 Compl;eted

---
+++ Image Processing/6 - Morphological Operation.py
@@ -0,0 +1,25 @@
+import cv2
+import numpy as np
+
+import matplotlib.pyplot as plt
+
+img = cv2.imread("..\dataset\imgs\letter1.png")
+
+#Kernal 5x5 Matrix 
+
+kernal = np.ones((5,5),np.uint8)
+
+erosion = cv2.erode(img,kernel=kernal,iterations=1)
+dilation = cv2.dilate(img,kernel=kernal,iterations=1)
+
+plt.subplot(131)
+plt.imshow(img)
+plt.title("Original Image")
+
+plt.subplot(132)
+plt.imshow(erosion)
+plt.title("Eroded Image")
+
+plt.subplot(133)
+plt.imshow(dilation)
+plt.title("Dilated Image")
