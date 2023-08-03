797042bd0a0c28ec5a44d7241f6918fcf9f943a3
KeerthiVasan.ai <keerthi77459@gmail.com>
2023-08-03 12:38:22 +0530

    Exercise 2 Completed

--- Image Processing/2 - Edge Correction.py
+++ Image Processing/2 - Edge Correction.py
@@ -3,7 +3,7 @@ import cv2
 from matplotlib import pyplot as plt
 
 filter = np.array([[-1,0,1],[-2,0,2],[-1,0,1]])
-img = cv2.imread(".\dataset\imgs\monkey.jpg",1)
+img = cv2.imread("..\dataset\imgs\monkey.jpg",1)
 
 sobel_vertical_img = cv2.filter2D(img,-1,filter)
 sobel_hori_img = cv2.filter2D(img,-1,np.flip(filter.T,axis=0))
