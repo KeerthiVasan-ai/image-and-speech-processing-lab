7db03b58b491214b3afc86173aef77142ea5254a
KeerthiVasan.ai <keerthi77459@gmail.com>
2023-08-17 12:20:03 +0530

    Copyright Issue

--- Image Processing/7 - Feature Extraction (SURF).py
+++ Image Processing/7 - Feature Extraction (SURF).py
@@ -2,15 +2,4 @@ import numpy as np
 import cv2
 from matplotlib import pyplot as plt
 
-img = cv2.imread("./dataset/imgs/ship.jpg",0)
-plt.imshow(img)
-
-surf = cv2.xfeatures2d.SIFT_create(200)
-kp, des = surf.detectAndCompute(img,None)
-# kp, des = surf.detectAndCompute(img,None)
-print(len(kp))
-# print(surf.getHessianThreshold())
-
-# surf.setHessainThreshold(50000)
-# kp,des = surf.detectAndCompute(img,None)
-# print(len(kp))
+# Code Will be Shortly Availale
